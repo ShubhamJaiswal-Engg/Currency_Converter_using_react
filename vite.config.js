86822deeac523a2785978669c476f3bc2b2e4e5c
Shubham Jaiswal <shubhamjai8662@gmail.com>
2026-05-27 14:16:27 +0530

    api response

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/frankfurter': {
+        target: 'https://api.frankfurter.dev',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/frankfurter/, '/v1'),
+      },
+    },
+  },
+  preview: {
+    proxy: {
+      '/frankfurter': {
+        target: 'https://api.frankfurter.dev',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/frankfurter/, '/v1'),
+      },
+    },
+  },
 })
