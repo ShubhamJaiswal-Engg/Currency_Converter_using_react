47c4730d363fa962639a50870409077c6b240a5a
Shubham Jaiswal <shubhamjai8662@gmail.com>
2026-05-27 14:10:30 +0530

    Setting weight full

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import CurrencyConverter from './components/CurrencyConverter'
 function App() {
 
   return (
-    <div className='min-h-screen bg-gray-100 flex flex-col items-center justify-center'>
-      <div className='container'>
-      <CurrencyConverter/>
+    <div className='min-h-screen bg-gray-100 flex flex-col items-center justify-center px-4 py-10'>
+      <div className='w-full'>
+        <CurrencyConverter />
       </div>
     </div>
   )
